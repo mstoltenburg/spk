b2c08d45d183775afcd48b7cb6d78bb7490feed5
Moritz Stoltenburg <mstoltenburg@metadesign.de>
2012-12-03 01:04:47 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Schilddrüsenpraxis Berlin Kantstraße</title>
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+	<header>
+		<div>
+			<h1>Schilddrüsenpraxis Kantstraße</h1>
+			<ul class="clearfix">
+				<li>Innere Medizin</li>
+				<li>Hormonlabor</li>
+				<li>Szintigrafie</li>
+			</ul>
+		</div>
+
+		<nav>
+			<ul class="clearfix">
+				<li><a href="team">Team</a></li>
+				<li><a href="aerztinnen">Ärztinnen</a></li>
+				<li><a href="schwerpunkt">Schwerpunkt</a></li>
+				<li><a href="termine">Termine</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<div id="content">
+
+	</div>
+
+	<div id="global">
+		<div class="clearfix">
+			<aside>
+				<h2 class="visuallyhidden">Adresse</h2>
+				<p>
+					Kantstraße<br>
+					10623 Berlin<br>
+					Tel. 030 / 313 30 53<br>
+					Fax. 003 / 313 37 61<br>
+				</p>
+			</aside>
+
+			<aside>
+				<h2 class="visuallyhidden">Sprechzeiten</h2>
+				<p>
+					Montag-Donnerstag:<br>
+					8-13 / 14-18 Uhr<br>
+					Freitag:<br>
+					8-13 Uhr<br>
+				</p>
+			</aside>
+
+			<aside>
+				<h2 class="visuallyhidden">Fahrverbindungen</h2>
+				<p>
+					Bus M49, U- / S-Bhf. Zoologischer Garten
+				</p>
+			</aside>
+		</div>
+	</div>
+
+	<footer>
+		<nav>
+			<ul class="clearfix">
+				<li><a href="impressum">Impressum</a></li>
+				<li><a href="kontakt">Kontakt</a></li>
+			</ul>
+		</nav>
+	</footer>
+
+</body>
+
+</html>
