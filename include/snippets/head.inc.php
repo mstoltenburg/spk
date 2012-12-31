9035ba6f861e9d1cb91479f0f648bd90733813e7
Moritz Stoltenburg <mstoltenburg@metadesign.de>
2012-12-31 16:54:55 +0100

    .

--- include/snippets/head.inc.php
+++ include/snippets/head.inc.php
@@ -2,5 +2,6 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<title>Schilddrüsenpraxis Berlin Kantstraße</title>
 	<meta name="viewport" content="width=device-width">
+	<link rel="shortcut icon" href="<?php echo $BASE_URL; ?>favicon.ico" type="image/x-icon" />
 	<link rel="stylesheet" href="<?php echo $BASE_URL; ?>css/normalize.css">
 	<link rel="stylesheet" href="<?php echo $BASE_URL; ?>css/main.css">
